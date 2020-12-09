773f591e3048cc8fc8dfc8eb866b2fe04034ee9d
mrpal39 <rp9545416@gmail.com>
2020-12-09 12:40:43 +0530

    Creating feeds for your blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# import  genriceView
 from .views import (
     PostListView,
     PostDetailView,
@@ -9,12 +8,17 @@ from .views import (
     # UserPostListView
 )
 from . import views
+
+from .feeds import  LatestPostsFeed
+
 urlpatterns = [
     path('', views.home, name='home'),
     # path('blog/', views.PostListView.as_view(), name='post_list'),
     path('blog/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='about'),
     path('<int:post_id>/share/',views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    
     path('blog/', views.post_list, name='post_list'),
     path('<int:year>/<slug:post>/',
         views.post_detail,
